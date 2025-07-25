1c27424febaedb68bcf280df62ba89bbfe92ae76
Kade Lieder <klieder03@gmail.com>
2025-07-25 19:31:41 -0400

    Added readme. Added CLI arguments.

--- CC/pkg/main.go
+++ CC/pkg/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"CC/pkg/client"
+	"flag"
+	"fmt"
+	"os"
 )
 
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return false
+}
+
 func main() {
-	// server.StartDNS()
-	// client.StartClient2()
-	// sleep()
-	client.StartClient1()
+
+	filepath := flag.String("file", "", "File to exfiltrate")
+	serverIP := flag.String("ip", "", "IP to send to")
+	flag.Parse()
+
+	if fileExists(*filepath) == false {
+		fmt.Printf("File \"%s\" not found.\n", *filepath)
+	}
+	fmt.Println(*serverIP)
+
+	//client.StartClient1()
 }
