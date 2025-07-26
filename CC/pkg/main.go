686597450efee4b61e106c6104ad41e9243cd530
wormmm <masonw2505@gmail.com>
2025-07-26 13:24:24 -0400

    Updated domain list

--- CC/pkg/main.go
+++ CC/pkg/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"CC/pkg/client"
-	"time"
 )
 
 func main() {
 	// server.StartDNS()
 	// client.StartClient2()
 	// sleep()
-	client.StartSender()
-	time.Sleep(time.Second * 3)
+	// client.StartSender()
+	// time.Sleep(time.Second * 3)
 	client.StartReceiver()
 }
